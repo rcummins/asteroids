dd2619f35e46113077d19c4564e7e3e41254a988
Renata Cummins <renatacummins@gmail.com>
2019-08-08 13:30:17 -0400

    feature: add randomVec method to utilities module

--- src/util.js
+++ src/util.js
@@ -2,6 +2,15 @@ const Util = {
   inherits(childClass, parentClass) {
     childClass.prototype = Object.create(parentClass.prototype);
     childClass.prototype.constructor = childClass;
+  },
+
+  randomVec(length) {
+    const angle = 2 * Math.PI * Math.random();
+    return Util.scale([Math.sin(angle), Math.cos(angle)], length);
+  },
+
+  scale(vector, m) {
+    return [vector[0] * m, vector[1] * m];
   }
 };
 
